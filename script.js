cd7cb9f6bda3a796c17d45ea9ed0817968564a29
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-05-16 18:27:14 +0200

    render emails on html

--- script.js
+++ script.js
@@ -4,3 +4,27 @@
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const app = Vue.createApp({
+    data(){
+        return{
+            arrEmails: [],
+        };
+    },
+
+    methods: {
+        getRandomEmail(){
+            for (let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(serverRandom => this.arrEmails.push(serverRandom.data.response)) 
+            }
+            
+            this.arrEmails = []
+        }
+    },
+
+    created(){
+        this.getRandomEmail()
+    },
+});
+
+app.mount('#app');
